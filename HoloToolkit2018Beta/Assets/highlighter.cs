57e940c601294b2319eb4580a51ad7f8b4414c8b
lp942014 <45824120+lp942014@users.noreply.github.com>
2019-03-24 15:04:45 -0400

    starting challenge 4

---
+++ HoloToolkit2018Beta/Assets/highlighter.cs
@@ -0,0 +1,23 @@
+﻿using Holokit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//forc caches rendering component
+[RequireComponent(typeof(Renderer))]
+public class highlighter : MonoBehaviour, IFocusable
+{
+    public Color initialColor;
+    public Color highlightColor;
+
+    private Renderer rendererInstance;
+    private Material materialInstance;
+
+    private void Awake()
+    {
+        //cache and initialize color, renderer, and meterial
+        rendererInstance = GetComponent<Renderer>();
+        materialInstance = rendererInstance.material;
+        initialColor = materialInstance.color;
+    }
+}
